59617774f6a57a84c9e7fc2d41c07a455e0c413d
Adriano Brito de Mendonça <adrianoskiazy51@gmail.com>
2022-02-24 12:44:01 -0300

    Up de exercícios

---
+++ origamid/Modulo-02/03- foreach_e_arrow_functions/script.js
@@ -0,0 +1,25 @@
+//Mostre no console cada parágrado do site
+const p = document.querySelectorAll('p')
+
+p.forEach(index => console.log(index))
+
+//Mostre o texto dos parágrafos no console
+
+const p = document.querySelectorAll('p')
+
+p.forEach(index => console.log(index.innerText))
+
+
+//Corrija os erros abaixo
+const imgs = document.querySelectorAll('img')
+
+imgs.forEach((item, index) => {
+    console.log(item, index)
+})
+
+let i = 0
+
+imgs.forEach(() => {
+    console.log(i++)
+})
+
