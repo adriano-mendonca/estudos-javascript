3b67b39a38767d8dc634553ea396e203131328d7
Adriano Brito de Mendonça <62914181+abm-astro@users.noreply.github.com>
2022-04-01 10:49:59 -0300

    Up de exercícios

---
+++ origamid/Modulo-02/20-object/script.js
@@ -0,0 +1,30 @@
+// Crie uma função que verifique corretamente o tipo de dado
+
+function verificar(dado){
+    return Object.prototyp.toString.call(dado)
+}
+// Crie um objeto quadrado com a propriedade lados e torne ela imutável
+
+const quadrado = {}
+
+Object.defineProperties(quadrado, {
+    lados: {
+        value: 4,
+        enumerable: true,
+    }
+})
+
+// Previna qualquer mudança no objeto abaixo
+
+const configuracao = {
+    width: 800,
+    height: 600,
+    background: '#333'
+}
+
+Object.freeze(configuracao))
+
+// Liste o nome de todas as propriedades do protótipo de String e Array
+
+console.log(Object.getOwnPropertyNames(String.prototype))
+console.log(Object.getOwnPropertyNames(Array.prototype))
